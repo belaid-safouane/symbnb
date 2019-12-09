f058e717d70a679f32eea1046cce6f31698ecdd5
BELAID <safouane.belaid@capgemini.com>
2019-12-09 16:14:43 +0100

    17. Création d'une page de liste des réservations

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -163,4 +163,14 @@ class AccountController extends Controller
             'user'=>$this->getUser()
         ]);
     }
+/**
+ * Permet d'afficher la liste des réservations faites par l'utilisateur
+ * 
+ * @Route("/account/bookings" , name="account_bookings")
+ * 
+ * @return Response 
+ */
+    public function bookings(){
+        return $this->render('account/bookings.html.twig');
+    }
 }
